befd3b1b0aac4d606c15b9cf7bf6b2f812d518d9
storres20 <sanchotorres777@gmail.com>
2021-07-09 00:49:10 -0500

    botones active con GET

--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -26,23 +26,72 @@
     <div class="container-fluid bg-light">
       <div class="container">
         <div class="nav nav-justified py-2 nav-pills">
-        <!--GET: $_GET["variable"] Variables que se pasan como parametros Via URL ( Tambien conocido como cadena de consulta a traves de la URL). 
+
+        <?php if(isset($_GET["pagina"])): ?>
+          
+          <?php if($_GET["pagina"] == "registro"): ?>
+            <li class="nav-item">
+            <a class="nav-link active" href="index.php?pagina=registro">Registro</a>
+          </li>
+          <?php else: ?>
+            <li class="nav-item">
+            <a class="nav-link" href="index.php?pagina=registro">Registro</a>
+          </li>
+          <?php endif ?>
+
+          <?php if($_GET["pagina"] == "ingreso"): ?>
+            <li class="nav-item">
+            <a class="nav-link active" href="index.php?pagina=ingreso">Ingreso</a>
+          </li>
+          <?php else: ?>
+            <li class="nav-item">
+            <a class="nav-link" href="index.php?pagina=ingreso">Ingreso</a>
+          </li>
+          <?php endif ?>
+
+          <?php if($_GET["pagina"] == "inicio"): ?>
+            <li class="nav-item">
+            <a class="nav-link active" href="index.php?pagina=inicio">Inicio</a>
+          </li>
+          <?php else: ?>
+            <li class="nav-item">
+            <a class="nav-link" href="index.php?pagina=inicio">Inicio</a>
+          </li>
+          <?php endif ?>
+
+          <?php if($_GET["pagina"] == "salir"): ?>
+            <li class="nav-item">
+            <a class="nav-link active" href="index.php?pagina=salir">Salir</a>
+          </li>
+          <?php else: ?>
+            <li class="nav-item">
+            <a class="nav-link" href="index.php?pagina=salir">Salir</a>
+          </li>
+          <?php endif ?>
+
+          <?php else: ?>
+
+            <!--GET: $_GET["variable"] Variables que se pasan como parametros Via URL ( Tambien conocido como cadena de consulta a traves de la URL). 
         Cuando es la primera variable, se separa con ? 
         las que siguen a continuacion , se separan con &
         -->
 
           <li class="nav-item">
-            <a class="nav-link" href="index.php?pagina=registro">Registro</a>
+            <a class="nav-link active" href="index.php?pagina=registro">Registro</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="index.php?pagina=ingreso">Ingreso</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link active" href="index.php?pagina=inicio">Inicio</a>
+            <a class="nav-link" href="index.php?pagina=inicio">Inicio</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="index.php?pagina=salir">Salir</a>
           </li>
+
+        <?php endif ?>
+
+        
         </div>
       </div>
     </div>
