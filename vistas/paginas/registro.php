4d2f7c494cf8c7394b54e644371c497d8c87a1fd
storres20 <sanchotorres777@gmail.com>
2021-07-09 09:53:48 -0500

    se agrego icono para los inputs del Formulario registro

--- vistas/paginas/registro.php
+++ vistas/paginas/registro.php
@@ -1,23 +1,38 @@
 <form>
     <div class="form-group mb-3">
         <label for="nombre" class="form-label">Nombre</label>
-
+        <!--Insert ICON Bootstrap - start-->
         <div class="input-group mb-3">
-
-        <div class="input-group-prepend">
-            <span class="input-group-text"><i class="bi bi-person-fill" style="font-size: 1rem; color: cornflowerblue;"></i></span>
+            <div class="input-group-prepend">
+                <span class="input-group-text"><i class="bi bi-person-fill" style="font-size: 1rem; color: cornflowerblue;"></i></span>
+            </div>
+            <input type="text" class="form-control" id="nombre"> 
         </div>
-            <input type="text" class="form-control" id="nombre">
-        </div>
-        
+        <!--Insert ICON Bootstrap - end-->
     </div>
+
     <div class="form-group mb-3">
         <label for="email" class="form-label">Correo electronico</label>
-        <input type="email" class="form-control" id="email">
+        <!--Insert ICON Bootstrap - start-->
+        <div class="input-group mb-3">
+        <div class="input-group-prepend">
+            <span class="input-group-text"><i class="bi bi-envelope-fill" style="font-size: 1rem; color: cornflowerblue;"></i></span>
+        </div>
+            <input type="email" class="form-control" id="email">
+        </div>
+        <!--Insert ICON Bootstrap - end-->
     </div>
+
     <div class="form-group mb-3">
         <label for="pwd" class="form-label">Contraseña</label>
-        <input type="password" class="form-control" id="pwd">
+        <!--Insert ICON Bootstrap - start-->
+        <div class="input-group mb-3">
+        <div class="input-group-prepend">
+            <span class="input-group-text"><i class="bi bi-lock-fill" style="font-size: 1rem; color: cornflowerblue;"></i></span>
+        </div>
+            <input type="password" class="form-control" id="pwd">
+        </div>
+        <!--Insert ICON Bootstrap - end-->
     </div>
 
     <button type="submit" class="btn btn-primary">Enviar</button>
