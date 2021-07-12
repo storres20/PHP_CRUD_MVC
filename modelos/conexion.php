ba71b97ea215e89830846e24da0c17404f32d373
storres20 <sanchotorres777@gmail.com>
2021-07-12 16:40:30 -0500

    conexion a mysql remote - CleverCloud

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -10,8 +10,11 @@ class Conexion{
         # Local Connection
         /* $link = new PDO("mysql:host=localhost; dbname=curso-php", "root", ""); */
         
-        # Remote Connection
-        $link = new PDO("mysql:host=remotemysql.com; dbname=T0bzSbEUSz", "T0bzSbEUSz", "3EWxoimUV1");
+        # Remote Connection - remoteMysql
+        /* $link = new PDO("mysql:host=remotemysql.com; dbname=T0bzSbEUSz", "T0bzSbEUSz", "3EWxoimUV1"); */
+        
+        # Remote Connection - clevercloud
+        $link = new PDO("mysql:host=beadirub92exfstjx9on-mysql.services.clever-cloud.com; dbname=beadirub92exfstjx9on", "urj5wc9vcrnkygvg", "FWzZwlCU4XhjcnBpO4Mi");
         
         $link -> exec("set names utf8");
         
