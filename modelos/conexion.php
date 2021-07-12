4d458dd7ac44ce049b836fe1e3355b66838f9054
storres20 <sanchotorres777@gmail.com>
2021-07-12 12:06:17 -0500

    se agrego Remote Connection to database

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -6,8 +6,13 @@ class Conexion{
     
         #PDO("nombre del servidor; nombre de la base de datos","nombre del usuario","contraseña")
         #Para la conexion a la Base de datos
-    
-        $link = new PDO("mysql:host=localhost; dbname=curso-php", "root", "");
+        
+        # Local Connection
+        /* $link = new PDO("mysql:host=localhost; dbname=curso-php", "root", ""); */
+        
+        # Remote Connection
+        $link = new PDO("mysql:host=remotemysql.com; dbname=T0bzSbEUSz", "T0bzSbEUSz", "3EWxoimUV1");
+        
         $link -> exec("set names utf8");
         
         return $link;
